f77ce04a27e23e0284813600b0d9c7563ecce509
JaPatGitHub <73636668+JaPatGitHub@users.noreply.github.com>
2021-06-13 09:41:10 +0530

    Converted to a function & renamed file
    Created a function called pointCoord()
    Tested at editor.p5js.org

---
+++ Tools/x-y-coordinate pointer.js
@@ -0,0 +1,9 @@
+// Define the function before setup() i.e. at the top
+// Call the function under draw()
+function pointCoord() {
+  stroke(255,0,0);
+  line(mouseX,0, mouseX, height);
+  line(0,mouseY, width, mouseY);
+  if (mouseIsPressed){print("x = "+ mouseX+ ", y = "+mouseY);}
+  }
+
