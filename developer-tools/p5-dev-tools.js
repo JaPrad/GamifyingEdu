c6db67edfc3992cce2bb3537b131296eb145c0c5
Jyotiraditya Pradhan <japsim6dev@gmail.com>
2022-04-03 10:46:30 +0530

    Ran prettier

--- developer-tools/p5-dev-tools.js
+++ developer-tools/p5-dev-tools.js
@@ -1,4 +1,3 @@
-
 // START
 // Call the function under draw()
 function showCoordinates() {
@@ -14,42 +13,55 @@ function showCoordinates() {
 // START
 // Call <object-name>.show() under draw()
 class Slider {
-  constructor(x, y, size, minValue, maxValue, step = 1, initialValue = minValue, lineColor = [255,255,255], circleColor = [0,0,0]){
-    this.initialX = x - size/2; // Starting x coordinate of line
-    this.finalX = x + size/2; // Ending x coordinate of line 
-    this.y = y
-    
+  constructor(
+    x,
+    y,
+    size,
+    minValue,
+    maxValue,
+    step = 1,
+    initialValue = minValue,
+    lineColor = [255, 255, 255],
+    circleColor = [0, 0, 0]
+  ) {
+    this.initialX = x - size / 2; // Starting x coordinate of line
+    this.finalX = x + size / 2; // Ending x coordinate of line
+    this.y = y;
+
     this.minValue = minValue;
     this.numOfSteps = floor((maxValue - minValue) / step); // Number of Steps
     this.stepSize = size / this.numOfSteps; // Length of Each step in pixels
-    
+
     this.value = initialValue;
-    
+
     this.lineColor = lineColor;
     this.circleColor = circleColor;
   }
-  
-  show(){
-    
-    strokeWeight(3)
-    stroke(...this.lineColor)
+
+  show() {
+    strokeWeight(3);
+    stroke(...this.lineColor);
     line(this.initialX, this.y, this.finalX, this.y);
-    
-    noStroke()
-    fill(...this.circleColor)
-    
-    if (mouseIsPressed && mouseX >= this.initialX && mouseX <= this.finalX+5 && mouseY >= this.y-10 && mouseY <= this.y+10){
-      
+
+    noStroke();
+    fill(...this.circleColor);
+
+    if (
+      mouseIsPressed &&
+      mouseX >= this.initialX &&
+      mouseX <= this.finalX + 5 &&
+      mouseY >= this.y - 10 &&
+      mouseY <= this.y + 10
+    ) {
       circle(mouseX, this.y, 10);
-      
-      this.value = this.minValue + floor((mouseX - this.initialX)/this.stepSize) // Value derived from slider
-    }
-    
-    else {
-      let circleX = this.initialX + ((this.value - this.minValue) * this.stepSize);
+
+      this.value =
+        this.minValue + floor((mouseX - this.initialX) / this.stepSize); // Value derived from slider
+    } else {
+      let circleX =
+        this.initialX + (this.value - this.minValue) * this.stepSize;
       circle(circleX, this.y, 10);
     }
   }
 }
 // END
-
